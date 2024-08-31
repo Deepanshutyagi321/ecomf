e215b7aad22a244d42f16782750a013afbe318ff
deepanshutyagi321 <deepanshuTyagi97208@gamil.com>
2024-08-31 20:09:01 +0530

    edit  /api

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-   proxy:{
-    '/api': 'http://localhost:8080',
-   }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://ecom-lgfu.onrender.com',
+        changeOrigin: true,
+        secure: true,  // Use this if your backend server uses HTTPS
+      },
+    },
   },
   plugins: [react()],
 })
